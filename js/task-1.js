456c7d50621a117db28305fd9645f9a4105d590f
AndreyKolo <https://github.com/AndreyKolo>
2020-03-10 16:28:45 +0200

    hw-3-01

---
+++ js/task-1.js
@@ -0,0 +1,27 @@
+// Напиши скрипт, который, для объекта user, последовательно:
+// добавляет поле mood со значением 'happy'
+// заменяет значение hobby на 'skydiving'
+// заменяет значение premium на false
+// выводит содержимое объекта user в формате ключ:значение используя Object.keys() и for...of
+
+'use strict';
+
+const user = {
+  name: 'Mango',
+  age: 20,
+  hobby: 'html',
+  premium: true,
+};
+console.log(user);
+
+user.mood = 'happy';
+
+user.hobby = 'skydiving';
+user['premium'] = 'false';
+console.log(user);
+
+const keys = Object.keys(user);
+
+for (const key of keys) {
+  console.log('Value: ', user[key]);
+}
